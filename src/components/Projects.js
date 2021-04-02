af495d0310b5c541c2ef2415182ae6caff04d7b4
Iancu Andrei <29271791+iancuandrei@users.noreply.github.com>
2021-04-02 22:46:34 +0300

    added todo project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./Projects.css";
 import devsource from "../images/devsource2-min.png";
 import artinsider from "../images/artinsider2-min.png";
-import travel from "../images/travel-min.png";
+import travel from "../images/travel.png";
+import todoapp from "../images/todoapp.png";
 import { FaGithubSquare, FaExternalLinkAlt } from "react-icons/fa";
 
 export default function Projects() {
@@ -15,7 +16,6 @@ export default function Projects() {
 					<h2>artInsider</h2>
 					<p>Art Gallery created using React.js and The Harvard Art Museums API.</p>
 					<div className="lang">
-						<div className="skill">HTML</div>
 						<div className="skill">CSS</div>
 						<div className="skill">JavaScript</div>
 						<div className="skill">React</div>
@@ -40,13 +40,12 @@ export default function Projects() {
 					</div>
 				</div>
 			</div>
-			<div className="project">
+			<div className="project reverse">
 				<img alt="project screenshot" src={travel}></img>
 				<div className="projDesc">
 					<h2>Travel From Your Couch Hackathon</h2>
 					<p>Project for the Travel from your couch Hackathon. Created with React, using a public live webcam API.</p>
 					<div className="lang">
-						<div className="skill">HTML</div>
 						<div className="skill">CSS</div>
 						<div className="skill">JavaScript</div>
 						<div className="skill">React</div>
@@ -92,6 +91,37 @@ export default function Projects() {
 							</button>
 						</a>
 						<a href="https://github.com/iancuandrei/devSource" rel="noopener noreferrer" target="_blank">
+							<button className="source">
+								{" "}
+								<FaGithubSquare className="btnIcon" />
+								Source Code
+							</button>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="project reverse">
+				<img class="todo" alt="project screenshot" src={todoapp}></img>
+				<div className="projDesc">
+					<h2>Todo App</h2>
+					<p>Todo App with Local Storage</p>
+					<div className="lang">
+						<div className="skill">React</div>
+						<div className="skill">CSS</div>
+						<div className="skill">JavaScript</div>
+						<div className="skill">Git</div>
+						<div className="skill">Netlify</div>
+						<div className="skill">MaterialUI</div>
+					</div>
+					<div className="buttons">
+						<a href="https://todo-app-react-material-ui.netlify.app/" rel="noopener noreferrer" target="_blank">
+							<button className="live">
+								{" "}
+								<FaExternalLinkAlt className="btnIcon" />
+								Live Demo
+							</button>
+						</a>
+						<a href="https://github.com/iancuandrei/todo-app-react" rel="noopener noreferrer" target="_blank">
 							<button className="source">
 								{" "}
 								<FaGithubSquare className="btnIcon" />
