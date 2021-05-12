937ecd2fe8f78cd0d6a0237550d5896033814b20
Iancu Andrei <29271791+iancuandrei@users.noreply.github.com>
2021-05-12 14:47:24 +0300

    seo h1

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -50,6 +50,7 @@ export default function LandingPage() {
 						</a>
 					</div>
 				</div>
+				<h1 style={{ display: "none" }}>Iancu Andrei - Full Stack JavaScript Developer</h1>
 				<h1>Hey, I'm Andrei 👋</h1>
 				<p>Full Stack JavaScript Developer</p>
 				<img className="portrait" alt="portrait" src={portrait}></img>
